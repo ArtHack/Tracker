0cb7d8fd9d377198b4aa07f948092ec34b83789d
Art Hack <art.hack.ios@gmail.com>
2023-05-05 10:46:39 +0300

    /

---
+++ Tracker/HabitViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class HabitViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Новая привычка"
+        view.backgroundColor = .white
+    }
+}
