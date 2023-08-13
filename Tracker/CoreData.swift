8eb91533568570ec9967d13e731c83b1653796bb
Art <art.hack.ios@gmail.com>
2023-08-13 09:50:28 +0300

    Made the initial settings

--- Tracker/CoreData.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CoreData.swift
-//  Tracker
-//
-//  Created by arthack on 08.08.2023.
-//
-
-import Foundation
--- Tracker/Marshalling.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Marshalling.swift
-//  Tracker
-//
-//  Created by arthack on 04.08.2023.
-//
-
-import Foundation
